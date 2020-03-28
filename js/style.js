4422cbe5ca6345f82fcc1d7221c129b5e1ab3208
pengxianghai <pengxianghai123@126.com>
2020-03-28 14:28:09 +0800

    question

--- js/style.js
+++ js/style.js
@@ -5,9 +5,13 @@ $(function () {
     });
     //over页面按钮
     $(".overback").click(function () {
-        window.location.href = "./../studyPage.html";
+        window.location.href = "studyPage.html";
     });
 
+    // 答题页面中点击返回按钮跳转到结束页
+    $('#quit').on('click', function () {
+        window.location.href = "over.html";
+    })
 
 
 }())
